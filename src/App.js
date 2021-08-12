f7f42a05933e70445037d7a8f06c614df702e535
Gaurav <gaurav.jain2@soprasteria.com>
2021-08-12 09:33:37 +0530

    ToDo List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+
+    const [inputList, setInputList] = useState("");
+    
+    const itemEvent = (event) =>
+    {
+       setInputList(event.target.value);
+    };
+
+
+   return (
+
+    <>
+    <div className = "main_div">
+    <div className = "center_div">
+
+       <br />
+       <h1> ToDo List </h1>
+       <br/>
+
+       <input type = "text" placeholder = "Add a Items" onChange = {itemEvent} />
+       <button > + </button>
+
+       <ol>
+           <li> {inputList} </li>
+       </ol>
+
+    </div>   
     </div>
-  );
-}
+
+    </>
+
+   )
+
+};
 
 export default App;
